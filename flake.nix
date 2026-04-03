97a6e04546c57f11a484a76d4ebb048b203d5b91
AsciiMoth <ascii@moth.contact>
2026-04-03 14:56:12 +0400

    chore: init cargo project

---
+++ flake.nix
@@ -0,0 +1,73 @@
+# Usage:
+#   nix profile add github:asciimoth/pineentry
+#   nix profile remove pineentry
+#   nix shell github:asciimoth/pineentry
+# Update: nix flake update
+{
+  description = "pineentry";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    naersk,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      naersk' = pkgs.callPackage naersk {};
+
+      checks = {
+        pre-commit-check = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            cargo-check.enable = true;
+            commitizen.enable = true;
+            typos.enable = true;
+            typos-commit = {
+              enable = true;
+              description = "Find typos in commit message";
+              entry = let script = pkgs.writeShellScript "typos-commit" ''
+                typos "$1"
+              ''; in builtins.toString script;
+              stages = [ "commit-msg" ];
+            };
+            cargo-test = {
+              enable = true;
+              name = "cargo test (workspace)";
+              entry = "cargo test --workspace --all-features";
+              pass_filenames = false;
+              stages = [ "pre-commit" ];
+            };
+          };
+        };
+      };
+
+      app = naersk'.buildPackage {
+        src = ./.;
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        inherit (checks.pre-commit-check) shellHook;
+        buildInputs = with pkgs; [
+          cargo
+
+          typos
+          commitizen
+        ];
+      };
+
+      packages.default = app;
+    });
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+const INITMSG: &str = r#"# PineEntry
+# A GNU pinentry caching proxy
+#
+# Src: https://github.com/asciimoth/pineentry
+# Config file: TODO
+# Usage: TODO
+#"#;
+
+fn main() {
+    println!("{}", INITMSG);
+}
